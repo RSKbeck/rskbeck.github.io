8e15c3bac191f3ee000a045f96311c29dc3113e1
RSKbeck <rkehlenb@calpoly.edu>
2017-02-12 23:44:30 -0800

    fixed error in last push

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
-		$("#submit").click(function(event){
-			var postData = {msg : $("#textbox").val()}
-			$.post("https://75.128.13.118:8082",
-        postData,
-        function(data){
-            $("#result").html(data);
+	$("#submit").click(function(event){
+		var postData = {msg : $("#textbox").val()};
+		$.post("https://75.128.13.118:8082",
+       		postData,
+       		function(data){
+        		$("#result").html(data);
         });
     });
-		});
-	});
+});
