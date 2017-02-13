3a28065c444d263c372023c3ff324952519f9878
RSKbeck <rkehlenb@calpoly.edu>
2017-02-12 20:52:43 -0800

    fixed json

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
-		$("#submit").click(function(event){
-			var jsonVal = JSON.stringify({ msg: $("#textbox").val() });    
-			$.post( "https://75.128.13.118:8082", { msg: $("#textbox").val() }, function( data ) {
+		$("#submit").click(function(event){  
+			$.post( "https://75.128.13.118:8082", { "msg": $("#textbox").val() }, function( data ) {
 				$( "#result" ).html( data );
 			}, "json");
 		});
