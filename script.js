605c14caccaaae27f1301e3cfe159309966341d6
RSKbeck <rkehlenb@calpoly.edu>
2017-02-12 23:53:35 -0800

    fixed style and url

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$("#submit").click(function(event){
 		var postData = {msg : $("#textbox").val()};
-		$.post("https://75.128.13.118:8082",
+		$.post("https://pi.rskbeck.com:8082",
        		postData,
        		function(data){
         		$("#result").html(data);
