a33c7f0a14b52394430533a66a14a8016a7b5e9b
RSKbeck <rkehlenb@calpoly.edu>
2017-02-12 20:57:27 -0800

    hopefully the final attempt to fix the json

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
 		$("#submit").click(function(event){
-			console.log({msg: "test"});
-			console.log(JSON.stringify({msg: "test"}, null, 2));
-			$.post( "https://75.128.13.118:8082", { "msg": $("#textbox").val() }, function( data ) {
+			$.post( "https://75.128.13.118:8082", JSON.stringify({ "msg": $("#textbox").val() }, null, 2), function( data ) {
 				$( "#result" ).html( data );
 			}, "application/json");
 		});
