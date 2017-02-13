a8684fe63b0f4cf75fe0d64b01c7da57c7d46f41
RSKbeck <rkehlenb@calpoly.edu>
2017-02-12 23:40:47 -0800

    finally found working command for post

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
 		$("#submit").click(function(event){
-			$.post( "https://75.128.13.118:8082", { msg: $("#textbox").val()}, function( data ) {
-				$( "#result" ).html( data );
-			}, "json");
+			var postData = {msg : $("#textbox").val()}
+			$.post("https://75.128.13.118:8082",
+        postData,
+        function(data){
+            $("#result").html(data);
+        });
+    });
 		});
 	});
