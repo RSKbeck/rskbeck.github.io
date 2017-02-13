beb263f7a84b11ffaf5b9c197ad1f51f20a74067
RSKbeck <rkehlenb@calpoly.edu>
2017-02-12 19:37:24 -0800

    fixed ip

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 		$("#submit").click(function(event){
-			$.post( "http:// 75.128.13.118:8081", $("#textbox").val(), function( data ) {
+			$.post( "https://75.128.13.118:8081", $("#textbox").val(), function( data ) {
 				$( "#result" ).html( data );
 			});
 		});
