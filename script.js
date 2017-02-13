2ceac55067b2ef10e3e45d09f764c5b6ab3f49ec
RSKbeck <rkehlenb@calpoly.edu>
2017-02-12 21:09:29 -0800

    fixed error

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 		$("#submit").click(function(event){
-			$.post( "https://75.128.13.118:8082", { msg: $("#textbox").val(), null, 2), function( data ) {
+			$.post( "https://75.128.13.118:8082", { msg: $("#textbox").val()}, function( data ) {
 				$( "#result" ).html( data );
 			}, "json");
 		});
