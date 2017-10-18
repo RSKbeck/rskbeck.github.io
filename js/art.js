c5928006ce051f041a290d863baa3dda079f31a9
RSKbeck <rskbeck@gmail.com>
2017-10-18 15:54:07 -0700

    Filled art page

--- js/art.js
+++ js/art.js
@@ -13,10 +13,10 @@ function showcol(urrl, col) {
 }
 
 $('#clipcover').click(function() {
-	show("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/clip.PNG\' \/>");
+	showcol("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/clip.PNG\' \/>", "#000");
 });
 $('#cutcover').click(function() {
-	show("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/cut2.PNG\' \/>");
+	showcol("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/cut2.PNG\' \/>", "#000");
 });
 $('#mintcover').click(function() {
 	show("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/mint.png\' \/>");
@@ -28,28 +28,28 @@ $('#poptcover').click(function() {
 	show("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/tez.png\' \/>");
 });
 $('#popgcover').click(function() {
-	show("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/dripopco.png\' \/>");
+	showcol("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/dripopco.png\' \/>", "#EFE4D4");
 });
 $('#morphcover').click(function() {
-	show("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/melt.gif\' \/>");
+	showcol("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/melt.gif\' \/>", "#333");
 });
 $('#glitchcover').click(function() {
 	show("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/good.png\' \/>");
 });
 $('#dropcover').click(function() {
-	show("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/drop.png\' \/>");
+	showcol("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/drop.png\' \/>", "#000");
 });
 $('#handcover').click(function() {
-	show("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/hand2.png\' \/>");
+	showcol("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/hand2.png\' \/>", "#000");
 });
 $('#skullcover').click(function() {
-	show("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/skull2.png\' \/>");
+	showcol("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/skull2.png\' \/>", "#000");
 });
 $('#splcover').click(function() {
-	show("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/splash.png\' \/>");
+	showcol("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/splash.png\' \/>", "#000");
 });
 $('#cocover').click(function() {
-	show("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/cococo.jpg\' \/>");
+	showcol("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/cococo.jpg\' \/>", "#001854");
 });
 $('#starcover').click(function() {
 	show("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/swag.bmp\' \/>");
