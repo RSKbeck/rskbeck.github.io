e5a5c49184c12a623e9d23b1301be6125108d366
RSKbeck <rskbeck@gmail.com>
2017-10-18 15:22:31 -0700

    Filled art page

--- js/art.js
+++ js/art.js
@@ -12,11 +12,45 @@ function showcol(urrl, col) {
   $('#fader').animate({opacity:1},1000);
 }
 
+$('#clipcover').click(function() {
+	show("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/clip.PNG\' \/>");
+});
+$('#cutcover').click(function() {
+	show("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/cut2.PNG\' \/>");
+});
+$('#mintcover').click(function() {
+	show("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/mint.png\' \/>");
+});
+$('#popcover').click(function() {
+	show("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/chopo.png\' \/>");
+});
+$('#poptcover').click(function() {
+	show("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/tez.png\' \/>");
+});
+$('#popgcover').click(function() {
+	show("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/dripopco.png\' \/>");
+});
 $('#morphcover').click(function() {
-	showcol("<img src=\'https:\/\/rskbeck.000webhostapp.com\/melt.gif\' \/>", "#333");
+	show("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/melt.gif\' \/>");
 });
-
 $('#glitchcover').click(function() {
-	show("<img src=\'https:\/\/rskbeck.github.io\/img\/2.jpg\' \/>");
+	show("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/good.png\' \/>");
 });
-
+$('#dropcover').click(function() {
+	show("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/drop.png\' \/>");
+});
+$('#handcover').click(function() {
+	show("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/hand2.png\' \/>");
+});
+$('#skullcover').click(function() {
+	show("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/skull2.png\' \/>");
+});
+$('#splcover').click(function() {
+	show("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/splash.png\' \/>");
+});
+$('#cocover').click(function() {
+	show("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/cococo.jpg\' \/>");
+});
+$('#starcover').click(function() {
+	show("<img src=\'https:\/\/rskbeck.github.io\/img\/art\/swag.bmp\' \/>");
+});
