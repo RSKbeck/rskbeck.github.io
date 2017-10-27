9728bb7660819561abbb75dd65a5f8c3391e5262
RSKbeck <rskbeck@gmail.com>
2017-10-26 20:11:38 -0700

    Added wolf and cany pages

--- js/game.js
+++ js/game.js
@@ -1,3 +1,12 @@
 $('#wbscover').click(function() {
 	$('#scroller').load('https://rskbeck.github.io/pages/games/wbs.htm');
 });
+
+$('#wolfcover').click(function() {
+	$('#scroller').load('https://rskbeck.github.io/pages/games/wolf.htm');
+});
+
+$('#canycover').click(function() {
+	$('#scroller').load('https://rskbeck.github.io/pages/games/cany.htm');
+});
+
