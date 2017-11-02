7bba941f74409341fcaa29a85d76bf84a72fe551
RSKbeck <rkehlenb@calpoly.edu>
2017-11-02 16:11:50 -0700

    Added proj pages

--- js/proj.js
+++ js/proj.js
@@ -1,3 +1,8 @@
 $('#stylocover').click(function() {
 	$('#scroller').load('https://rskbeck.github.io/pages/projs/stylo.htm');
+	$('#scroller').scrollTop(0);
+});
+$('#cvbrmcover').click(function() {
+	$('#scroller').load('https://rskbeck.github.io/pages/projs/cvbrm.htm');
+	$('#scroller').scrollTop(0);
 });
