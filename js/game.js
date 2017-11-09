f511a79f1ceb9f2fddd801abe082486ee25d5030
RSKbeck <rskbeck@gmail.com>
2017-11-09 15:15:34 -0800

    Added fragment id support

--- js/game.js
+++ js/game.js
@@ -1,19 +1,23 @@
 $('#wbscover').click(function() {
+	window.location.hash = 'wbs';
 	$('#scroller').load('https://rskbeck.github.io/pages/games/wbs.htm');
 	$('#scroller').scrollTop(0);
 });
 
 $('#wolfcover').click(function() {
+	window.location.hash = 'wolf';
 	$('#scroller').load('https://rskbeck.github.io/pages/games/wolf.htm');
 	$('#scroller').scrollTop(0);
 });
 
 $('#canycover').click(function() {
+	window.location.hash = 'cany';
 	$('#scroller').load('https://rskbeck.github.io/pages/games/cany.htm');
 	$('#scroller').scrollTop(0);
 });
 
 $('#ctfcover').click(function() {
+	window.location.hash = 'wip';
 	$('#scroller').load('https://rskbeck.github.io/pages/games/wip.htm');
 	$('#scroller').scrollTop(0);
 });
--- js/proj.js
+++ js/proj.js
@@ -1,8 +1,10 @@
 $('#stylocover').click(function() {
+	window.location.hash = 'stylo';
 	$('#scroller').load('https://rskbeck.github.io/pages/projs/stylo.htm');
 	$('#scroller').scrollTop(0);
 });
 $('#cvbrmcover').click(function() {
+	window.location.hash = 'cvbrm';
 	$('#scroller').load('https://rskbeck.github.io/pages/projs/cvbrm.htm');
 	$('#scroller').scrollTop(0);
 });
